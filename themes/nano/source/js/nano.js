094f487c8a8f956ba6587b8f1793ebf7a7e8909c
ray0324 <ray0324@foxmail.com>
2018-08-22 17:41:46 +0800

    update

--- themes/nano/source/js/nano.js
+++ themes/nano/source/js/nano.js
@@ -3,3 +3,13 @@ var data = {
   b:2
 }
 console.log(data);
+hljs.configure({
+})
+
+$(document).ready(function(){
+  console.log('ready');
+  $('pre code').each(function (i, block) {
+    
+    hljs.highlightBlock(block);
+  });
+})
